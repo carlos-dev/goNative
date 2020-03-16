b18202c6a8ab7c87ba55cf90bcc281512d7f520b
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2020-03-16 00:02:37 -0300

    introduzindo o axios

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 import {StatusBar} from 'react-native';
 
+import api from '../../services/api';
+
 import {Container, Input, Button, ButtonText, Error} from './styles';
 
 export default class Login extends Component {
   state = {username: ''};
 
-  handleSubmit = () => {
+  handleSubmit = async () => {
     const {username} = this.state;
+
+    try {
+      await api.get(`/users/${username}`).then(data => console.log(data));
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render() {
