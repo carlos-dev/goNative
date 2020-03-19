9547b8bc671506553b09c86e407e5f6660b866e5
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2020-03-18 21:03:09 -0300

    estilixando a página de respositóries e carreegando os repositórios

--- src/pages/repositories/index.js
+++ src/pages/repositories/index.js
@@ -1,12 +1,44 @@
+/* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {ActivityIndicator, Text} from 'react-native';
 
-export default class Repositories extends Component {
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as RepositoriesActions from '../../store/actions/repositories';
+
+import {Container} from './styles';
+
+class Repositories extends Component {
+  componentDidMount() {
+    const {loadRepositoriesRequest} = this.props;
+    console.log(this.props);
+
+    loadRepositoriesRequest();
+  }
+
   render() {
+    const {repositories} = this.props;
+    console.log(repositories);
+
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
+      <Container>
+        {repositories.loading ? (
+          <ActivityIndicator size="small" color="#999" />
+        ) : (
+          repositories.data.map(repository => (
+            <Text key={repository.id}>{repository.name}</Text>
+          ))
+        )}
+      </Container>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  repositories: state.repositories,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(RepositoriesActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Repositories);
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,23 +1,39 @@
-import {all, takeLatest, call, put} from 'redux-saga/effects';
+import {all, takeLatest, call, put, select} from 'redux-saga/effects';
+
 import api from '../../services/api';
+import {navigate} from '../../services/navigation';
 
 import * as LoginActions from '../actions/login';
-function* login(action) {
-  console.log(action);
+import * as RepositoriesActions from '../actions/repositories';
 
+function* login(action) {
   try {
     const {user} = action.payload;
 
     yield call(api.get, `/users/${user}`);
     yield put(LoginActions.loginSuccess(user));
 
-    // navigation.navigate('Repositories');
+    navigate('Repositories');
   } catch (error) {
     yield put(LoginActions.loginFailure());
     console.log(error);
   }
 }
 
+function* loadRepositories() {
+  try {
+    const {username} = yield select(state => state.login);
+
+    const response = yield call(api.get, `/users/${username}/repos`);
+    yield put(RepositoriesActions.loadRepositoriesSuccess(response.data));
+  } catch (error) {
+    yield put(RepositoriesActions.loadRepositoriesFailure());
+  }
+}
+
 export default function* rootSaga() {
-  return yield all([takeLatest('LOGIN_REQUEST', login)]);
+  return yield all([
+    takeLatest('LOGIN_REQUEST', login),
+    takeLatest('LOAD_REPOSITORIES_REQUEST', loadRepositories),
+  ]);
 }
