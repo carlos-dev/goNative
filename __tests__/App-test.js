f4af89e14c3aac68b21ea3abd0a5c532d2960ec8
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2020-03-19 00:38:13 -0300

    testes com enzyme

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,13 +2,13 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
-import App from '../App';
-
+import Login from '../src/pages/login';
+import {mount, shallow} from 'enzyme';
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const wrapper = shallow(<Login />);
+
+  expect(wrapper.find('Container').exists()).toBe(true);
 });
